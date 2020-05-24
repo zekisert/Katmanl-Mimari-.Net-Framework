293d7bc10358c013547b086b68b84ecde13fcfe7
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-24 18:22:09 +0300

    ProductDetail.cs

---
+++ Entities/Complex Types/ProductDetail.cs
@@ -0,0 +1,16 @@
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ComplexTypes
+{
+    public class ProductDetail : IEntity
+    {
+        public  int ProductId { get; set; }
+        public  string ProductName { get; set; }
+        public string CategoryName  { get; set; }
+    }
+}
