2cff3a0e2bd88cde41dac1b836b51f2250ed1d47
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-24 18:11:06 +0300

    IProductDal.cs

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+using Core.DataAccess;
+using Entities.ComplexTypes;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        List<ProductDetail> GetProductDetails();
+    }
+}
